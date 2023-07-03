f92b6cd534f1fd106359c2f9a9205fc500ab2253
Bolukan <github@bolukan.nl>
2023-07-03 22:39:51 +0200

    Update gitignore for compile files

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "67"
+  #define BUILD_NUMBER "69"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.0.230703_67 - 2023-07-03 21:25:25.043172"
+  #define VERSION "v1.0.230703_69 - 2023-07-03 22:38:37.468238"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.0.230703_67"
+  #define VERSION_SHORT "v1.0.230703_69"
 #endif

44ec51d6187d3044ca888ae4af6426cd81ecd547
Bolukan <github@bolukan.nl>
2023-07-04 12:54:54 +0200

    Add example version.h

---
+++ include/version.h
@@ -0,0 +1,10 @@
+
+#ifndef BUILD_NUMBER
+  #define BUILD_NUMBER "80"
+#endif
+#ifndef VERSION
+  #define VERSION "v1.0.230704_80 - 2023-07-04 12:54:10.067438"
+#endif
+#ifndef VERSION_SHORT
+  #define VERSION_SHORT "v1.0.230704_80"
+#endif
